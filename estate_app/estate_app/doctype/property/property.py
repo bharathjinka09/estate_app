924ae8fd71b7bf905fdf0826ebe948dc60df1d48
bharath <bharath.ranga@medleymed.com>
2021-06-20 17:08:20 +0530

    Frappe Controller, ORM and SQL in - ERPNext

--- estate_app/estate_app/doctype/property/property.py
+++ estate_app/estate_app/doctype/property/property.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2021, bharath and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Property(Document):
-	pass
+	def after_insert(self):
+		frappe.msgprint(f"Document <b>{self.name}</b> inserted successfully!")
+	# validate
+	def validate(self):
+		if(self.property_type=="Apartment"):
+			for amenity in self.amenitiess:
+				if(amenity.amenity=="Outdoor Kitchen"):
+					frappe.throw(f"Property of type <b>Apartment</b> shouldn't have amenity <b>{amenity.amenity}</b>")
+			
+			# or SQL Query
+			# amenity = frappe.db.sql(f"""SELECT amenity FROM `tabProperty Amenity Detail` WHERE parent='{self.name}' AND parenttype='Property' AND amenity='Outdoor Kitchen'""",as_dict=True)
+			# print(amenity,"Amenity.......")
+			# if(amenity):
+			# 	frappe.throw(f"Property of type <b>Apartment</b> shouldn't have amenity <b>{amenity[0].amenity}</b>")
