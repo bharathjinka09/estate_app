b7ccb03e24768920249a135ff4934d6e9a3d2ac8
bharath <bharath.ranga@medleymed.com>
2021-06-21 16:26:11 +0530

    Frappe API Calls

--- estate_app/estate_app/doctype/property/property.js
+++ estate_app/estate_app/doctype/property/property.js
@@ -46,7 +46,32 @@ frappe.ui.form.on('Property', {
 					frappe.msgprint(`Address updated with ${value} successfully!`)
 				}
 			})
-		})
+		},"Actions");
+
+		// check property types
+		frm.add_custom_button('Check Property Types', () => {
+			let property_type = frm.doc.property_type
+			console.log(property_type)
+			// make ajax call
+			frappe.call({
+				method: "estate_app.estate_app.doctype.property.api.check_property_types",
+				args: {"property_type": property_type},
+				callback: function(r){
+					console.log(r,"API")
+					if(r.message.length>0){
+						let header = `<h3>Below property is of type ${property_type}</h3>`;
+						let body = '';
+						r.message.forEach(d=>{
+							let cont = `<p>Name: ${d.name}: <a href='/app/property/${d.name}'>Visit</a></p>`
+							body += cont
+						})
+						let all = header + body
+						// message print
+						frappe.msgprint(all)
+					}
+				}
+			})
+		},"Actions")
 	},
 });
 
