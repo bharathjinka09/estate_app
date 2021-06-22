a8b5d412f58b432e8119342a6a07e4fb05edccd5
bharath <bharath.ranga@medleymed.com>
2021-06-22 13:12:05 +0530

    Doctype Event - Dynamically Create Document in ERPNext

--- estate_app/estate_app/doctype/property/events.py
+++ estate_app/estate_app/doctype/property/events.py
@@ -1,6 +1,23 @@
 import frappe
 
 def validate(doc,event):
-	print(f"\n\n\n\n{doc}:{event}")
-	frappe.throw("Error occured!")
+	# print(f"\n\n\n\n{doc}:{event}")
+	# frappe.throw("Error occured!")
+	pass
+
+def on_update(doc,event):
+	print(f"\n\n\n\n{doc}:{event}","doc : event")
+	frappe.msgprint(f"{doc.name} has been updated by {doc.owner}")
+	
+def after_insert(doc,event):
+	# create note on property insert
+	note = frappe.get_doc({
+		'doctype':'Note',
+		'title':f"{doc.name} Added",
+		'public':True,
+		'content': doc.description
+	})
+	note.insert()
+	frappe.db.commit()
+	frappe.msgprint(f"{note.title} has been created!")
 	
--- estate_app/hooks.py
+++ estate_app/hooks.py
@@ -101,13 +101,10 @@ doctype_js = {
 # Hook on document methods and events
 
 doc_events = {
-	"*": {
-		"on_update": "method",
-		"on_cancel": "method",
-		"on_trash": "method"
-	},
 	"Property":{
 		"validate":"estate_app.estate_app.doctype.property.events.validate",
+		"on_update":"estate_app.estate_app.doctype.property.events.on_update",
+		"after_insert":"estate_app.estate_app.doctype.property.events.after_insert",
 	}
 }
 
