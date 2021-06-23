901e1fe88286210c6058d18376f556776119a67a
bharath <bharath.ranga@medleymed.com>
2021-06-23 12:48:08 +0530

    Display doctype list on detail page with related properties

--- estate_app/www/property/detail.py
+++ estate_app/www/property/detail.py
@@ -6,6 +6,9 @@ def get_context(context):
 		url_param = frappe.form_dict['docname']
 		context.property = frappe.get_doc("Property",url_param)
 		context.agent = frappe.get_doc("Agent",context.property.agent)
+		related_properties = frappe.db.sql(f"""SELECT creation, name, property_name, status, address, grand_total,image FROM `tabProperty` WHERE property_type='{context.property.property_type}' AND name != '{context.property.name}' ORDER BY creation DESC LIMIT 3;""", as_dict=True)
+		context.related_properties = related_properties
+
 	except Exception as e:
 		frappe.local.flags.redirect_location = '/404'
 		raise frappe.Redirect
--- estate_app/www/property/index.py
+++ estate_app/www/property/index.py
@@ -3,7 +3,7 @@ import frappe
 
 def get_context(context):
 	properties = frappe.db.sql("""
-		SELECT name,property_name,status,address,grand_total,image FROM `tabProperty`;""",
+		SELECT name,property_name,status,address,grand_total,image FROM `tabProperty` ORDER BY creation DESC;""",
 		as_dict=True)
 	print(properties,"sql query")
 	context.properties = properties
